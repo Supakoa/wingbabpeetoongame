28299d3e422b8d8494136236c26cf48fb32af681
Perzival1997 <s59122519028@ssru.ac.th>
2018-11-26 18:45:18 +0700

    Vat

--- src/wingbabpeetoon/Wingbabpeetoon.java
+++ src/wingbabpeetoon/Wingbabpeetoon.java
@@ -16,7 +16,7 @@ public class Wingbabpeetoon {
      */
     public static void main(String[] args) {
         System.out.println("hellow");
-        System.out.println(">>>>>");
+        System.out.println(">>>>>a");
     }
     
 }
